7cdef82d9cfe9312fde0070d6dde512dc5acbf35
Renucci Stella-Maria <stella-maria.renucci@etu.umontpellier.fr>
2022-05-12 16:50:33 +0200

    Ajout d'un bouton passer (vérification du fonctionnement effectuée)

--- src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
@@ -1,7 +1,12 @@
 package fr.umontpellier.iut.vues;
 
 import fr.umontpellier.iut.IJeu;
-import javafx.scene.layout.Pane;
+import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
@@ -12,21 +17,37 @@ import javafx.scene.layout.Pane;
  * (le joueur courant, les 5 cartes Wagons visibles, les destinations lors de l'étape d'initialisation de la partie, ...)
  * ainsi que les listeners à exécuter lorsque ces éléments changent
  */
-public class VueDuJeu extends Pane {
+public class VueDuJeu extends VBox {
 
     private IJeu jeu;
     private VuePlateau plateau;
 
+    private EventHandler<ActionEvent> gestionnaireEvenement;
+
+    private Button boutonPasser;
+    private Label nomJoueurCourant;
+
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
         plateau = new VuePlateau();
-        getChildren().add(plateau);
+
+        nomJoueurCourant = new Label();
+        boutonPasser = new Button("Passer");
+
+        getChildren().add(boutonPasser);
+        getChildren().add(nomJoueurCourant);
+        //getChildren().add(plateau);
     }
 
     public IJeu getJeu() {
         return jeu;
     }
 
-    public void creerBindings() {}
+    public void creerBindings() {
+        boutonPasser.setOnAction((event) -> {
+            jeu.passerAEteChoisi();
+            nomJoueurCourant.setText(jeu.joueurCourantProperty().getValue().getNom());
+        });
+    }
 
 }
